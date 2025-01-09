691cd247fde178819acfe3d973bcf59f1c5ef69a
phansytuan <tuanphanthc@gmail.com>
2025-01-09 20:09:39 +0700

    create servlet

---
+++ servlet/employee-manager/src/main/java/vn/t3h/employeemanager/HelloServlet.java
@@ -0,0 +1,31 @@
+package vn.t3h.employeemanager;
+
+import java.io.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        System.out.println("HelloServlet init");
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+    // sử dụng để update dữ liệu trên server
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    }
+
+    public void destroy() {
+    }
+}
